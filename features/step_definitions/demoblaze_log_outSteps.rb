1205288e4cb4844830f4519636a9a1450a83aef7
ASalinasVe <alejandra.salinas@ucb.edu.bo>
2026-05-31 14:14:48 -0400

    fix: wait of feature steps log out

--- features/step_definitions/demoblaze_log_outSteps.rb
+++ features/step_definitions/demoblaze_log_outSteps.rb
@@ -1,9 +1,9 @@
 Then('the welcome message should be visible before logout') do
   expected_message = "Welcome #{ENV['DEMOBLAZE_USER']}"
 
-  expect(page).to have_selector(:css, '#nameofuser', wait: 10)
+  expect(page).to have_selector(:css, '#nameofuser', wait: 2)
 
-  actual_message = find(:css, '#nameofuser', wait: 10).text
+  actual_message = find(:css, '#nameofuser', wait: 2).text
 
   puts "***WELCOME MESSAGE BEFORE LOGOUT: #{actual_message}"
 
@@ -13,9 +13,9 @@ Then('the welcome message should be visible before logout') do
 end
 
 When('I click on Log out for logout validation') do
-  expect(page).to have_selector(:css, '#logout2', wait: 10)
+  expect(page).to have_selector(:css, '#logout2', wait: 2)
 
-  find(:css, '#logout2', wait: 10).click
+  find(:css, '#logout2', wait: 2).click
 
   sleep 3
 
@@ -23,19 +23,19 @@ When('I click on Log out for logout validation') do
 end
 
 Then('the Log in option should be visible after logout') do
-  expect(page).to have_selector(:css, '#login2', wait: 10)
+  expect(page).to have_selector(:css, '#login2', wait: 2)
 
   puts "***LOG IN OPTION IS VISIBLE"
 end
 
 Then('the Sign up option should be visible after logout') do
-  expect(page).to have_selector(:css, '#signin2', wait: 10)
+  expect(page).to have_selector(:css, '#signin2', wait: 2)
 
   puts "***SIGN UP OPTION IS VISIBLE"
 end
 
 Then('the welcome message should not be visible after logout') do
-  expect(page).to have_no_selector(:css, '#nameofuser', wait: 10)
+  expect(page).to have_no_selector(:css, '#nameofuser', wait: 2)
 
   puts "***WELCOME MESSAGE IS NOT VISIBLE AFTER LOGOUT"
 end
