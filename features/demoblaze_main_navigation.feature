7e7e480d9ff9694ed882ff7e9fc73f70c3af8e50
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2026-06-13 02:20:03 +0200

    refactor: mejorar main navigation con lenguaje natural
    - Cambiar 'main navigation result' por 'I see the section'
    - Usar lenguaje natural: 'click on from the menu'
    - Eliminar tecnicismos: 'main navigation'
    - Mejorar redaccion desde perspectiva de usuario

--- features/demoblaze_main_navigation.feature
+++ features/demoblaze_main_navigation.feature
@@ -1,21 +1,21 @@
-Feature: Demoblaze main navigation validation
-              As a demo user
-              I want to validate the main menu options
-  So that I can confirm that each navigation option responds correctly
+Feature: Demoblaze main menu options
+  As a shopper
+  I want to use the main menu options
+  So that I can navigate to different sections of the store
 
-        Background:
-            Given I browse to Demoblaze page
+  Background:
+    Given I am on the Demoblaze home page
 
-        @main_navigation_validation
-        Scenario Outline: Validate main navigation menu option
-             When I click the "<menu_option>" main navigation option
-             Then the "<menu_option>" main navigation result should be displayed
+  @main_navigation_validation
+  Scenario Outline: Navigate using the main menu
+    When I click on "<menu_option>" from the menu
+    Then I see the "<menu_option>" section
 
-        Examples:
-                  | menu_option |
-                  | Home        |
-                  | Contact     |
-                  | About us    |
-                  | Cart        |
-                  | Log in      |
-                  | Sign up     |
+    Examples:
+      | menu_option |
+      | Home        |
+      | Contact     |
+      | About us    |
+      | Cart        |
+      | Log in      |
+      | Sign up     |
--- features/step_definitions/demoblaze_main_navigationSteps.rb
+++ features/step_definitions/demoblaze_main_navigationSteps.rb
@@ -1,29 +1,11 @@
-When('I click the {string} main navigation option') do |menu_option|
-  puts "***CLICKING MAIN NAVIGATION OPTION: #{menu_option}"
-
-  case menu_option
-  when 'Home'
-    click_link('Home')
-  when 'Contact'
-    click_link('Contact')
-  when 'About us'
-    click_link('About us')
-  when 'Cart'
-    click_link('Cart')
-  when 'Log in'
-    click_link('Log in')
-  when 'Sign up'
-    click_link('Sign up')
-  else
-    raise "Unknown menu option: #{menu_option}"
-  end
-
+When('I click on {string} from the menu') do |menu_option|
+  puts "***CLICKING MENU OPTION: #{menu_option}"
+  click_link(menu_option)
   sleep 2
-
-  puts "***CLICKED MAIN NAVIGATION OPTION: #{menu_option}"
+  puts "***CLICKED: #{menu_option}"
 end
 
-Then('the {string} main navigation result should be displayed') do |menu_option|
+Then('I see the {string} section') do |menu_option|
   case menu_option
   when 'Home'
     valid_urls = [
@@ -36,71 +18,56 @@ Then('the {string} main navigation result should be displayed') do |menu_option|
       'https://www.demoblaze.com/#',
       'https://www.demoblaze.com/index.html#'
     ]
-
-    unless valid_urls.include?(current_url)
-      raise "Home navigation failed. Current URL: #{current_url}"
-    end
-
+    expect(valid_urls).to include(current_url)
     expect(page).to have_selector(:xpath, "//a[contains(@class, 'hrefch')]", wait: 10)
-
-    puts "***HOME PAGE DISPLAYED CORRECTLY"
-    puts "***CURRENT URL: #{current_url}"
+    puts "***HOME PAGE DISPLAYED"
 
   when 'Contact'
     modal = find(:css, '#exampleModal', visible: true, wait: 10)
     expect(modal).to be_visible
-
     within('#exampleModal') do
       expect(page).to have_content('New message')
       expect(page).to have_selector('#recipient-email', wait: 10)
       expect(page).to have_selector('#recipient-name', wait: 10)
       expect(page).to have_selector('#message-text', wait: 10)
     end
-
-    puts "***CONTACT MODAL DISPLAYED CORRECTLY"
+    puts "***CONTACT FORM DISPLAYED"
 
   when 'About us'
     modal = find(:css, '#videoModal', visible: true, wait: 10)
     expect(modal).to be_visible
-
     within('#videoModal') do
       expect(page).to have_content('About us')
       expect(page).to have_selector('video, iframe, .video-js', wait: 10)
     end
-
-    puts "***ABOUT US MODAL DISPLAYED CORRECTLY"
+    puts "***ABOUT US SECTION DISPLAYED"
 
   when 'Cart'
     expect(page).to have_current_path('/cart.html', wait: 10)
     expect(page).to have_selector('#tbodyid', wait: 10)
-
-    puts "***CART PAGE DISPLAYED CORRECTLY"
+    puts "***CART PAGE DISPLAYED"
 
   when 'Log in'
     modal = find(:css, '#logInModal', visible: true, wait: 10)
     expect(modal).to be_visible
-
     within('#logInModal') do
       expect(page).to have_content('Log in')
       expect(page).to have_selector('#loginusername', wait: 10)
       expect(page).to have_selector('#loginpassword', wait: 10)
     end
-
-    puts "***LOG IN MODAL DISPLAYED CORRECTLY"
+    puts "***LOG IN FORM DISPLAYED"
 
   when 'Sign up'
     modal = find(:css, '#signInModal', visible: true, wait: 10)
     expect(modal).to be_visible
-
     within('#signInModal') do
       expect(page).to have_content('Sign up')
       expect(page).to have_selector('#sign-username', wait: 10)
       expect(page).to have_selector('#sign-password', wait: 10)
     end
-
-    puts "***SIGN UP MODAL DISPLAYED CORRECTLY"
+    puts "***SIGN UP FORM DISPLAYED"
 
   else
-    raise "Unknown menu option validation: #{menu_option}"
+    raise "Unknown menu option: #{menu_option}"
   end
 end
